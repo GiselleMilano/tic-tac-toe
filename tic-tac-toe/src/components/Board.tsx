f27899bbf075b144b799ea5183ad62470c1bda2f
ReginaEfull <32282517+GiselleMilano@users.noreply.github.com>
2023-01-27 12:52:40 -0300

    game completed

---
+++ tic-tac-toe/src/components/Board.tsx
@@ -0,0 +1,90 @@
+import { MarkOptions } from "perf_hooks";
+import { useState } from "react";
+import "../index.css";
+
+type mark = "x" | "o" | null;
+
+interface Props {
+  value: mark;
+  onSquareClick: () => void;
+}
+
+function Square(props: Props) {
+  return (
+    <button onClick={props.onSquareClick} className="board-box">
+      {props.value}
+    </button>
+  );
+}
+
+function calculateWinner(squares: Array<string>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null)); // Define un array con 9 elementos, cada uno de ellos es null.
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status: string;
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  function handleClick(i: number) {
+    const nextSquares = squares.slice();
+    console.log(nextSquares);
+    if (squares[i] || winner) {
+      return;
+    }
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="board-content">
+      <div className="text-lg">{status}</div>
+      <div className="board-row">
+        <Square onSquareClick={() => handleClick(0)} value={squares[0]} />
+        <Square onSquareClick={() => handleClick(1)} value={squares[1]} />
+        <Square onSquareClick={() => handleClick(2)} value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square onSquareClick={() => handleClick(3)} value={squares[3]} />
+        <Square onSquareClick={() => handleClick(4)} value={squares[4]} />
+        <Square onSquareClick={() => handleClick(5)} value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square onSquareClick={() => handleClick(6)} value={squares[6]} />
+        <Square onSquareClick={() => handleClick(7)} value={squares[7]} />
+        <Square onSquareClick={() => handleClick(8)} value={squares[8]} />
+      </div>
+    </div>
+  );
+}
